3f0282d16279ad552030c6366ea7b049fe6f8b97
Marin Cabac <marin.cabac@gmail.com>
2018-02-23 15:37:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   end
   #######################################
   def save
-     class.all << self
+     self.class.all << self
   end
   #######################################
   def self.create_by_name(name_of_s)
