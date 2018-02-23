c4bebd26da7211fb0f1b5e3acbd162c91c734d39
Marin Cabac <marin.cabac@gmail.com>
2018-02-23 15:22:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,6 +52,6 @@ end
   end
   #######################################
   def self.create_from_filename(mp3_name)
-   @@all << self.new_from_filename(mp3_name) 
+     self.new_from_filename(mp3_name)
     #initializes and saves a song and artist_name based on the filename format (FAILED - 1)
 end
